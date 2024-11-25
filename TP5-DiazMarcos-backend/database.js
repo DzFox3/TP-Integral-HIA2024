981bdc284b48c88302a0e8743a3f631dcbb36a60
DzFox3 <diazmarcos4444@gmail.com>
2024-11-25 14:18:01 -0300

    Commit Final?

--- TP5-DiazMarcos-backend/database.js
+++ TP5-DiazMarcos-backend/database.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
-const URI = process.env.MONGO_URI || "mongodb://mongo:27017/tp5db";
+const URI =
+  process.env.MONGO_URI ||
+  "mongodb://mongo1,mongo2,mongo3:27017/tp5db?replicaSet=myReplicaSet";
 //const URI = "mongodb://0.0.0.0/tp5db";
 mongoose
   .connect(URI)
